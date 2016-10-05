ef083f582e908e9794c7b4f469ed9f0f4f47777a
pedro paulo travassos <pedropaulotravassos@gmail.com>
2016-10-05 15:50:15 -0300

    testando

--- CadastroVeiculoDesk/Pim.Patriot.CadVecDesk/Program.cs
+++ CadastroVeiculoDesk/Pim.Patriot.CadVecDesk/Program.cs
@@ -14,6 +14,7 @@ namespace Pim.Patriot.CadVecDesk
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmCadVec());
--- CadastroVeiculoDesk/Pim.Patriot.CadVecDesk/frmCadVec.Designer.cs
+++ CadastroVeiculoDesk/Pim.Patriot.CadVecDesk/frmCadVec.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(112, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
             // 
             // frmCadVec
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "frmCadVec";
             this.Text = "Cadastro de veiculos";
             this.ResumeLayout(false);
@@ -42,5 +59,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
