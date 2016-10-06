beb4db382c15f32151807f261edf4c2fd2124d94
pedro paulo travassos <pedropaulotravassos@gmail.com>
2016-10-06 19:20:32 -0300

    Mundaças vindo

--- Pim.Patriot.ClassLibrary/Pim.Patriot.DataAccess/ClassesDAO/VeiculoDAO.cs
+++ Pim.Patriot.ClassLibrary/Pim.Patriot.DataAccess/ClassesDAO/VeiculoDAO.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,17 @@ namespace Pim.Patriot.DataAccess.ClassesDAO
 {
     public class VeiculoDAO
     {
+        #region Atributos e Propriedades
+        private string connectionString;
+        #endregion
+
+        SqlConnection conexao = new SqlConnection();
+        conexao.ConectionString = conectionString;
+        
+        
+
+
+        
+
     }
 }
