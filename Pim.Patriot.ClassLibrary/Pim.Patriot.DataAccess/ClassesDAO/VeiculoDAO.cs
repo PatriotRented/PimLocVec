9f3d2647dca860b53ed9ac09fcef732fd3084a84
pedro paulo travassos <pedropaulotravassos@gmail.com>
2016-10-10 20:32:41 -0300

    atualizei mais algumas coisas

--- Pim.Patriot.ClassLibrary/Pim.Patriot.DataAccess/ClassesDAO/VeiculoDAO.cs
+++ Pim.Patriot.ClassLibrary/Pim.Patriot.DataAccess/ClassesDAO/VeiculoDAO.cs
@@ -11,7 +11,35 @@ namespace Pim.Patriot.DataAccess.ClassesDAO
     public class VeiculoDAO
     {
         SqlConnection conexao = new SqlConnection();
-        
 
+        #region Metodos
+
+        /// <summary>
+        /// Insere um registro na tabela veiculo do banco de dados 
+        /// </summary>
+        /// <returns></returns>
+        public object insertVeiculo()
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Faz o update de um registro no banco de dados a partir do seu id
+        /// </summary>
+        /// <returns></returns>
+        public object updateVeiculo(long id) //Esse método só será implementado na tela de consulta de veículos (for now...)
+        {
+            throw new NotImplementedException();
+        }
+        
+        /// <summary>
+        /// Faz o select de todos os dados da tabela
+        /// </summary>
+        public void selectAllVec()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from veiculos";
+        }
+        #endregion
     }
 }
