6e1bf9bbe310d7038e743831cbe1f8b23350695f
Jean <jean.silva.papa@gmail.com>
2016-11-13 20:18:41 -0200

    seila eu kk

--- Pim.Patriot.ClassLibrary/Pim.Patriot.ClassLibrary/ClientePF.cs
+++ Pim.Patriot.ClassLibrary/Pim.Patriot.ClassLibrary/ClientePF.cs
@@ -2,6 +2,7 @@ using System;
 
 public class ClientePF : Cliente
 {
+    #region atributos
     private int codCli;
 
     private string nomeCli;
@@ -15,7 +16,7 @@ public class ClientePF : Cliente
     private string cnh;
 
 	private DateTime dt_nasc;
-
+    #endregion
 
     public override bool cadastrar()
 	{
@@ -28,3 +29,54 @@ public class ClientePF : Cliente
     }
 }
 
+#region Metodos
+public int cadCli(string _nomecli, string _email, string _cnh, string _cnpj_cpf, int _codEnd,
+    int _codTel,DateTime _dt_nasc )
+{
+    try
+    {
+        this.nomecli = _nomecli;
+        this.email = _email;
+        this.cnh = _cnh;
+        this.cnpj_cpf = _cnpj_cpf;
+        this.codEnd = _codEnd;
+        this.codTel = _codTel;
+
+        ConnectionFactory conn = new ConnectionFactory();
+        SqlConnection conexao = new SqlConnection(conn.pegaConexao("connSQL"));
+
+        SqlCommand cmdInsert = conexao.CreateCommand();
+        cmdInsert.CommandText =
+            @"Insert into Veiculo 
+                    (codCat, modelo, marca,cor, placa, statusVec)
+                values
+                    (@codCat,@modelo,@marca,@cor,upper(@placa),@statusVec);";
+
+        cmdInsert.Parameters.AddWithValue("@codcat", _codcat);
+        cmdInsert.Parameters.AddWithValue("@modelo", _modelo);
+        cmdInsert.Parameters.AddWithValue("@marca", _marca);
+        cmdInsert.Parameters.AddWithValue("@cor", _cor);
+        cmdInsert.Parameters.AddWithValue("@placa", _placa);
+        cmdInsert.Parameters.AddWithValue("@statusVec", 'D');
+
+        SqlCommand cmdSmax = conexao.CreateCommand();
+        cmdSmax.CommandText =
+            @"SELECT MAX(codVec) FROM Veiculo";
+
+        conexao.Open();
+
+        cmdInsert.ExecuteNonQuery();
+        this.codVec = Convert.ToInt32(cmdSmax.ExecuteScalar());
+
+        conexao.Close();
+
+        return this.codVec;
+    }
+    catch
+    {
+        Exception ex = new Exception();
+        MessageBox.Show(Convert.ToString(ex));
+        throw ex;
+    }
+
+}
